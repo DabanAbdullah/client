0903c88dcf4b3b3dfac6462e02a56282bed6c256
daban.s <daban.csi@gmail.com>
2024-01-06 14:42:19 +0100

    styled component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 export const Landing = () => {
-  return <div>Landing page</div>;
+  return (
+    <Wrapper>
+      <h1>Landing page</h1>
+      <div className="content">sjbdksjd</div>
+    </Wrapper>
+  );
 };
 
+const Wrapper = styled.div`
+  background-color: lightblue;
+  h1 {
+    color: red;
+  }
+  .content {
+    background-color: red;
+  }
+`;
 export default Landing;
